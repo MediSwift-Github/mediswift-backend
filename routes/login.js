a8fcd658a762b4538be9b5a762995b98e98e2855
Rushabh <rushabghoghari@gmail.com>
2024-07-14 22:19:37 +0530

    creaet and send to ken in backend

--- routes/login.js
+++ routes/login.js
@@ -1,29 +1,43 @@
 // routes/login.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../database/users'); // Make sure this path is correct
+const User = require("../database/users"); // Make sure this path is correct
+const jwt = require("jsonwebtoken");
 
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
-router.post('/api/login', async (req, res) => {
-    const { username, password } = req.body;
-    console.log(`Attempting to find user: ${username} with password: ${password}`); // Debug log
-    try {
-        const user = await User.findOne({ username: username });
-        console.log(user); // This logs null if user not found
+router.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  console.log(
+    `Attempting to find user: ${username} with password: ${password}`
+  ); // Debug log
+  try {
+    const user = await User.findOne({ _id: username });
+    console.log(user); // This logs null if user not found
 
-        if (user && user.password === password) {
-            // Authentication successful
-            res.json({ message: 'Authentication successful', role: user.role });
-        } else {
-            // Authentication failed
-            res.status(401).json({ message: 'Authentication failed' });
-        }
-    } catch (error) {
-        console.error('Login error:', error);
-        res.status(500).json({ message: 'An error occurred. Please try again later.' });
+    const token = createToken(user._id);
+
+    console.log("token-->>", token);
+
+    if (user && user.password === password) {
+      // Authentication successful
+      res.json({
+        message: "Authentication successful",
+        role: user.role,
+        token,
+      });
+    } else {
+      // Authentication failed
+      res.status(401).json({ message: "Authentication failed" });
     }
+  } catch (error) {
+    console.error("Login error:", error);
+    res
+      .status(500)
+      .json({ message: "An error occurred. Please try again later." });
+  }
 });
-
-
 
 module.exports = router;
